ae51bb46941f0936b473756a80ea138435ca97c9
Matias Garcia Isaia <mgarcia@manas.com.ar>
2015-03-20 20:02:47 -0300

    Parameterized URL and concept UUIDs

--- app/models/open_mrs_api.rb
+++ app/models/open_mrs_api.rb
@@ -2,11 +2,8 @@ require 'net/http'
 
 class OpenMrsApi
 
-  SAMPLE_ID_CONCEPT = "f9a40b7e-66c2-45d0-bbea-541e51dc2868"
-  SAMPLE_RESULT_CONCEPT = "80185920-5237-47e0-8c1f-af2ca0179bec"
-
   def search_patients name
-    uri = URI.parse("http://localhost:8081/openmrs-standalone/ws/rest/v1/patient?q=#{name}")
+    uri = URI.parse("#{Settings.openmrs_base_url}/patient?q=#{name}")
 
     req = Net::HTTP::Get.new(uri.request_uri)
     req.basic_auth 'admin', 'Admin123'
@@ -21,7 +18,7 @@ class OpenMrsApi
   end
 
   def search_encounters patient_uuid
-    uri = URI.parse("http://localhost:8081/openmrs-standalone/ws/rest/v1/encounter/?patient=#{patient_uuid}")
+    uri = URI.parse("#{Settings.openmrs_base_url}/encounter/?patient=#{patient_uuid}")
 
     req = Net::HTTP::Get.new(uri.request_uri)
     req.basic_auth 'admin', 'Admin123'
@@ -36,14 +33,14 @@ class OpenMrsApi
   end
 
   def report_sample patient_sample
-    uri = URI.parse("http://localhost:8081/openmrs-standalone/ws/rest/v1/obs/")
+    uri = URI.parse("#{Settings.openmrs_base_url}/obs/")
 
     req = Net::HTTP::Post.new(uri.request_uri)
     req.basic_auth 'admin', 'Admin123'
     req.body = {
       person: patient_sample.patient_uuid,
       obsDatetime: DateTime.now,
-      concept: SAMPLE_ID_CONCEPT,
+      concept: Settings.sample_id_concept_uuid,
       encounter: patient_sample.encounter_uuid,
       value: patient_sample.sample_id
     }.to_json
@@ -58,14 +55,14 @@ class OpenMrsApi
   end
 
   def register_result patient_sample, result
-    uri = URI.parse("http://localhost:8081/openmrs-standalone/ws/rest/v1/obs/")
+    uri = URI.parse("#{Settings.openmrs_base_url}/obs/")
 
     req = Net::HTTP::Post.new(uri.request_uri)
     req.basic_auth 'admin', 'Admin123'
     req.body = {
       person: patient_sample.patient_uuid,
       obsDatetime: DateTime.now,
-      concept: SAMPLE_RESULT_CONCEPT,
+      concept: Settings.sample_result_concept_uuid,
       encounter: patient_sample.encounter_uuid,
       value: result
     }.to_json
