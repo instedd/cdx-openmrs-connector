ecee5e653f6ed7501dd49b653da0755a502755d1
Santiago Palladino <spalladino@gmail.com>
2015-04-01 11:25:35 -0300

    Changed parameter name from sample to sample_id to match CDX format

--- app/controllers/open_mrs_api_controller.rb
+++ app/controllers/open_mrs_api_controller.rb
@@ -16,14 +16,16 @@ class OpenMrsApiController < ApplicationController
   end
 
   def register_result
-    sample_id = params[:sample]
+    sample_id = params[:sample_id]
     result = params[:result]
 
+    Rails.logger.info("Registering result #{result} for sample #{sample_id}")
     patient_sample = PatientSample.where(sample_id: sample_id).first
 
     if patient_sample
       render json: @api.register_result(patient_sample, result)
     else
+      Rails.logger.warn("Sample #{sample_id} not found")
       render json: {success: false, error: "Unknown Sample ID: #{sample_id}"}
     end
   end
